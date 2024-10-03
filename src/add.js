ab4300eb4245e2c48e4bfd6f482eaa360c3ee6ea
Carla Lopez <iamcarla.28@gmail.com>
2024-10-03 20:49:34 +0200

    Started lab

--- src/add.js
+++ src/add.js
@@ -5,3 +5,15 @@ function add (numOne, numTwo) {
 
     return numOne + numTwo;
 }
+
+/* 1. How many tests are there for the add function in the add.spec.js file?
+There are 4 tests. 
+2. How are the blocks describe and it being used in the tests? What is the purpose of each?
+"Describe" groups together related tests. I think is a way of creating "folders" and "subfolder"
+in the different tests to keep them organiced. 
+"It" mark each of the tests, they describe an specific behavior of the code. 
+3. How are the test descriptions phrased? Are there any keywords that stand out?
+The test desscriptions start with "It" and is followed with a phrase of what the code should do to p
+pass this test, like "should return, should be, should take"
+4. What do the expect functions do, and what input do they take?
+The expect functions do literally pose examples of what
